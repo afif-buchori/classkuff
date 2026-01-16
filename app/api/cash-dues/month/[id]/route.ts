82cf15d5b31fe0e9d2123bf05a987fb5c3f2aaa7
Afif Buchori <123707908+afif-buchori@users.noreply.github.com>
2026-01-16 11:39:26 +0800

    Update route.ts

--- app/api/cash-dues/month/[id]/route.ts
+++ app/api/cash-dues/month/[id]/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
-export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
+export async function GET(request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
     try {
         const { id } = await params;
 
